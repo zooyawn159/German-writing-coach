e6b81bade58530d6a3cf83d0dcc0b2178ba741e5
zooyawn159 <samstagsgeschenk@gmail.com>
2026-06-06 13:56:22 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // CORS
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -28,4 +27,4 @@ export default async function handler(req, res) {
 
   const text = data.choices?.[0]?.message?.content ?? '';
   res.status(200).json({ text });
-}
+};
